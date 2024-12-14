16566c7c5040593e477cea9e284d3fa2a74972d6
OfirGilad <ofirgila@post.bgu.ac.il>
2024-12-14 18:50:06 +0200

    tests in progress

--- mesh_builder.py
+++ mesh_builder.py
@@ -104,94 +104,86 @@ class MeshBuilder:
 #########
 # Tests #
 #########
-def test_coupler_xx(gg, output_path):
-    position1 = (0, 0, 0)
-    active_connections1 = ["x", "-x"]
-
-    position2 = (1, 0, 0)
-    active_connections2 = ["x", "-x"]
-
-    nodes_data = {
-        0: {
-            "position": position1,
-            "active_connections": active_connections1,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections1))
-        },
-        1: {
-            "position": position2,
-            "active_connections": active_connections2,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections2))
+def build_test_mesh(gg, positions, all_active_connections, output_path):
+    # Build nodes_data and position_to_node_map
+    nodes_data = {}
+    position_to_node_map = {}
+    num_of_nodes = len(positions)
+    for i in range(num_of_nodes):
+        nodes_data[i] = {
+            "position": positions[i],
+            "active_connections": all_active_connections[i],
+            "invalid_connections": list(set(gg.connection_types) - set(all_active_connections[i]))
         }
-    }
-    position_to_node_map = {
-        position1: 0,
-        position2: 1
-    }
+        position_to_node_map[i] = positions[i]
+
+    # Generate the graph
     G = gg.generate_graph_3d(nodes_data=nodes_data, position_to_node_map=position_to_node_map)
 
     # Build the mesh
     mb = MeshBuilder()
     mb.build_mesh(G=G, output_path=output_path)
+
+
+# Test Coupler
+def test_coupler_xx(gg, output_path):
+    positions = [(0, 0, 0), (1, 0, 0)]
+    all_active_connections = [["x", "-x"], ["x", "-x"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
 
 
 def test_coupler_yy(gg, output_path):
-    position1 = (0, 0, 0)
-    active_connections1 = ["y", "-y"]
+    positions = [(0, 0, 0), (0, 1, 0)]
+    all_active_connections = [["y", "-y"], ["y", "-y"]]
 
-    position2 = (0, 1, 0)
-    active_connections2 = ["y", "-y"]
-
-    nodes_data = {
-        0: {
-            "position": position1,
-            "active_connections": active_connections1,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections1))
-        },
-        1: {
-            "position": position2,
-            "active_connections": active_connections2,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections2))
-        }
-    }
-    position_to_node_map = {
-        position1: 0,
-        position2: 1
-    }
-    G = gg.generate_graph_3d(nodes_data=nodes_data, position_to_node_map=position_to_node_map)
-
-    # Build the mesh
-    mb = MeshBuilder()
-    mb.build_mesh(G=G, output_path=output_path)
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
 
 
 def test_coupler_zz(gg, output_path):
-    position1 = (0, 0, 0)
-    active_connections1 = ["z", "-z"]
+    positions = [(0, 0, 0), (0, 1, 0)]
+    all_active_connections = [["z", "-z"], ["z", "-z"]]
 
-    position2 = (0, 0, 1)
-    active_connections2 = ["z", "-z"]
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
 
-    nodes_data = {
-        0: {
-            "position": position1,
-            "active_connections": active_connections1,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections1))
-        },
-        1: {
-            "position": position2,
-            "active_connections": active_connections2,
-            "invalid_connections": list(set(gg.connection_types) - set(active_connections2))
-        }
-    }
-    position_to_node_map = {
-        position1: 0,
-        position2: 1
-    }
-    G = gg.generate_graph_3d(nodes_data=nodes_data, position_to_node_map=position_to_node_map)
 
-    # Build the mesh
-    mb = MeshBuilder()
-    mb.build_mesh(G=G, output_path=output_path)
+# Test Elbow
+def test_elbow_xy1(gg, output_path):
+    positions = [(0, 0, 0), (1, 0, 0), (1, 1, 0)]
+    all_active_connections = [["x", "-x"], ["x", "y"], ["y", "-y"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
+def test_elbow_xy2(gg, output_path):
+    positions = [(0, 0, 0), (1, 0, 0), (1, -1, 0)]
+    all_active_connections = [["x", "-x"], ["x", "-y"], ["y", "-y"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
+def test_elbow_xz1(gg, output_path):
+    positions = [(0, 0, 0), (1, 0, 0), (1, 0, 1)]
+    all_active_connections = [["x", "-x"], ["x", "z"], ["z", "-z"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
+def test_elbow_xz2(gg, output_path):
+    positions = [(0, 0, 0), (1, 0, 0), (1, 0, -1)]
+    all_active_connections = [["x", "-x"], ["x", "-z"], ["z", "-z"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
+def test_elbow_yz1(gg, output_path):
+    positions = [(0, 0, 0), (0, 1, 0), (0, 1, 1)]
+    all_active_connections = [["y", "-y"], ["y", "z"], ["z", "-z"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
+def test_elbow_yz2(gg, output_path):
+    positions = [(0, 0, 0), (0, 1, 0), (0, 1, -1)]
+    all_active_connections = [["y", "-y"], ["y", "-z"], ["z", "-z"]]
+
+    build_test_mesh(gg=gg, positions=positions, all_active_connections=all_active_connections, output_path=output_path)
+
 
 
 def tests():
@@ -207,6 +199,14 @@ def tests():
     test_coupler_xx(gg=gg, output_path=os.path.join(tests_path, "coupler_xx.obj"))
     test_coupler_yy(gg=gg, output_path=os.path.join(tests_path, "coupler_yy.obj"))
     test_coupler_zz(gg=gg, output_path=os.path.join(tests_path, "coupler_zz.obj"))
+
+    # Test Elbow
+    test_elbow_xy1(gg=gg, output_path=os.path.join(tests_path, "elbow_xy1.obj"))
+    test_elbow_xy2(gg=gg, output_path=os.path.join(tests_path, "elbow_xy2.obj"))
+    test_elbow_xz1(gg=gg, output_path=os.path.join(tests_path, "elbow_xz1.obj"))
+    test_elbow_xz2(gg=gg, output_path=os.path.join(tests_path, "elbow_xz2.obj"))
+    test_elbow_yz1(gg=gg, output_path=os.path.join(tests_path, "elbow_yz1.obj"))
+    test_elbow_yz2(gg=gg, output_path=os.path.join(tests_path, "elbow_yz2.obj"))
 
 
 if __name__ == '__main__':
